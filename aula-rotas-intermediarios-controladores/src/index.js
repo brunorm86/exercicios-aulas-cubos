5e2a3412d536ee6511072d545a357765e8a51ebf
Bruno Ricardo Machado <bruno_r_machado@estudante.sesisenai.org.br>
2023-08-16 22:48:39 -0300

    commit

--- aula-rotas-intermediarios-controladores/src/index.js
+++ aula-rotas-intermediarios-controladores/src/index.js
@@ -2,8 +2,38 @@ const express = require('express');
 
 const app = express();
 
+const professores = [
+    {
+        id: 1,
+        nome: "Guido",
+        stack: "Backend"
+    },
+    {
+        id: 2,
+        nome: "Bruno",
+        stack: "Frontend"
+    },
+    {
+        id: 3,
+        nome: "Diego",
+        stack: "Fullstack"
+    },
+    {
+        id: 4,
+        nome: "Astrogildo",
+        stack: "UX"
+    }
+];
+
 app.listen(3000);
 
-app.get('/', (req, res) =>{
-    res.send('Página inicial');
+app.get('/professores', (req, res) => {
+    res.send(professores);
+});
+
+app.get('/professores/:id', (req, res) => {
+    const professorEncontrado = professores.find((professor) => {
+        return professor.id == req.params.id;
+    });
+    res.send(professorEncontrado);
 })
